62d4a1bc42d1b65b7177f0da7cdbcce8286b7495
morninng <mr_morninng_star_dash@yahoo.co.jp>
2020-01-29 16:43:09 +0900

    interface added just for clarification

--- test-node/src/index.ts
+++ test-node/src/index.ts
@@ -3,6 +3,7 @@
 import http = require('http');
 import express = require('express');
 
+
 const serverPort = 3002;
 const serverHost = '127.0.0.1';
 
@@ -10,18 +11,16 @@ const app = express();
 const httpServer = http.createServer(app);
 const server = httpServer.listen(serverPort, serverHost, () => {});
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 
 import { HelloExec } from '../../functions/src/class/hello-exec';
+import { HelloIf } from '../../functions/model/hello';
 
 app.get('/testA', async(request, response) => {
 
   const hello = new HelloExec();
-  await hello.test();
-  return response.send('test all finished');
+  const aaa: HelloIf = {aaa: 'seees'};
+  await hello.test(aaa);
+return response.send('test all finished');
+
 });
 
